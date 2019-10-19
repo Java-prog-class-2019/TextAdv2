dd274eafd1b99ef532505ebe54846ac8731f000a
Carter Tomlenovich <cartertom@tuta.io>
2019-10-19 12:40:36 -0400

    New master

---
+++ src/components/Bonk.java
@@ -0,0 +1,30 @@
+package components;
+
+public class Bonk {
+	
+	boolean playerTurn;
+	boolean systemTurn;
+	
+	public static void main(String[] args) {
+		
+		new Bonk();
+		
+		}
+	
+	
+	void Bonk() {
+		init();
+		
+		
+	}
+	
+	public void init() {
+		System.out.println(" Welcome to Bonk! Type 'help' for a list of commands %n");
+		playerTurn = false;
+		systemTurn = true;
+		
+		
+	}
+	
+
+}
